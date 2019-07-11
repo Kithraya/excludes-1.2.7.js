dbf467c2ad869e943038969c5aec2adf3cb1e5bb
Kithraya <46088893+Kithraya@users.noreply.github.com>
2019-07-11 19:31:06 -0400

    Version 1.2.1

--- excludes.js
+++ excludes.js
@@ -1,72 +1,61 @@
+/** Excludes obsolete browsers (IE < 11), unsupported addEventListener etc. from entering the page 
+    by redirecting to a specified URL before your code runs
+    MIT license. https://github.com/Kithraya/excludes 
+    Tested in IE6+, Chrome, Firefox, Edge, Opera 10.6+, etc.
+*/
 
-// Excludes obsolete browsers (IE < 9), unsupported addEventListener etc. from entering your web page. MIT license. https://github.com/Kithraya/excludes
+// dropping support for IE5-10 leaves ~5 out of every 1000 clients high and dry. Acceptable.
 
-// if obsolete, change window.location to URL
+(function(win, doc, undefined) { // keep the global namespace unpolluted by wrapping in an IIFE
 
-(function(win, undefined) { // keep the global namespace unpolluted by wrapping in an IIFE
+	var version = "1.2.1";
+	var obsolete = false;
 	
-	function hasAttribute(elem, attr) { return (('hasAttribute' in elem) ? elem.hasAttribute(attr) : (typeof elem[attr] !== 'undefined'));  } // elem[attr] notation works for IE6/7
-	function getAttribute(elem, attr) {
-			
-		if (!hasAttribute(elem, attr)) { return null; } 
-		var attr = (elem.getAttribute) ? elem.getAttribute(attr) : 0; 
-		// getAttribute() seems to be always defined in all browsers, if not always implemented properly.
-		
-		if (typeof attr !== 'string') { // TODO: parse outerHTML. attr is an OBJECT if we query the style attribute in IE6/7, but we're not doing this anyway (yet).
-			return null;
-		}
-		
-		return attr;
-	}
+	var is_IE11 = !!( !('currentScript' in document) && window.msCrypto);
 	
-	var is_IE11 = !('currentScript' in document) && !!window.msCrypto; 
-	// msCrypto (with the prefix) is only defined in IE11 (for now).
-	// Source: https://developer.mozilla.org/en-US/docs/Web/API/Window/crypto 
-	// Shoutout to https://stackoverflow.com/users/5807141/j-j for the overlooked comment on msCrypto:
-	// https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
-	// And document.currentScript is not defined in any version of IE, but all versions of Edge support it.
-	// so we combine the checks to future proof it.
-	// If documentMode has been changed from the default, this check will return false,
-	// but @_jscript_version will return "11".
+	/*  msCrypto (with the prefix ms-) is only defined in IE11 (for now).
+	    Source - https://developer.mozilla.org/en-US/docs/Web/API/Window/crypto
+	    Shoutout to https://stackoverflow.com/users/5807141/j-j for the overlooked comment on msCrypto -
+	    https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
+	    And document.currentScript is not defined in any version of IE, but all versions of Edge support it.
+	    so we combine the checks to future proof it.
+	    If documentMode has been changed from the default, is_IE11 will be `false`, but @_jscript_version will return "11".
+	*/
+
+	var jscript_version = parseInt( new Function("/*@cc_on return @_jscript_version; @*/")() , 10); 
 	
-	var obsolete = (function(win,undefined) { // script tags if not async or defer are executed in the order they appear.
-		var t = true;
-		var min_version = 9; 
-		var isOldIE = (function() { 
-			// using handy conditional compilation which executes in IE only, and does not change with document mode.
-			var version = new Function("/*@cc_on return @_jscript_version; @*/")(); 
-			
-			// "5.6/7": IE6, "5.7": IE7, "5.8": IE8, "9":IE9, "10": IE10, "11": IE11 with an older document mode.
-			
-			return !!(version && parseInt( version, 10 ) < min_version);
-			})();
-		if (isOldIE || !document.addEventListener) { return t } // require IE > 8 and addEventListener support.
-		
-		// Technically just checking for addEventListener invalidates IE <= 8.
-		// To exclude IE9 and 10, change min_version to 11
-		// to exclude IE11 as well, change min_version to 12 and write
-		// if (is_IE11 || isOldIE) { return true }
-		
-		// you can add more custom logic here if required, just remember to return a Boolean.
-		
-		// if you want a finer-grained function that determines the exact version of IE, look at:
-		// https://github.com/Kithraya/detect-IE
-		
-	})(win);
+	// values before parseInt:
+	// NOT IE: undefined, IE6: "5.6" or "5.7", IE7: "5.7", IE8: "5.8", IE9: "9", IE10: "10", IE11 in an older document mode: "11"
+
+	/*  Conditional compilation (@cc_on) executes in IE only, and does not change with the document mode.
+	    Browsers other than IE will interpret this as `new Function("")()` which is the same as `function()()` which is `undefined`. 
+	    parseInt(undefined) returns NaN which is falsy
+	    by default, IE11 will not interpret conditional compilations, unless IE11 is in an older document mode
+	*/
+	
+	if ( jscript_version || !document.addEventListener ) { obsolete = true; }
+	// Do not allow IE11 operating in older document modes, else code may still break
+	
+	// to exclude IE 11 period, uncomment the following line: 
+	///	if (is_IE11 || jscript_version === 11 ) { obsolete = true; }
 	
 	var scripts = document.getElementsByTagName('script');
 	var attr, i, elem, allow;
 	if (obsolete) {
 		for (i=0; i < scripts.length; i++) {
 			elem = scripts[i];
-			attr = getAttribute(elem, 'data-get') || getAttribute(elem, 'get') || 
-             		       getAttribute(elem, 'data-fallback') || getAttribute(elem, 'fallback') || false;
-			if (attr) { 
-				window.location.href = attr; break;
+			if (elem.getAttribute) { 
+				// getAttribute is not foolproof in every situation but it's usable for our purposes here
+				attr = elem.getAttribute('data-redirect') || elem.getAttribute('redirect') || 
+				       elem.getAttribute('data-fallback') || elem.getAttribute('fallback') || false;
+				if (attr) {
+					window.location.href = attr; break;
+				}
+			} else {
+				window.location.href = 'https://google.co.uk'; 
+				// if you find a browser where getAttribute isn't supported, let me know
 			}
-			
-			
 		}
 	}
 
-})(window);
+})(window, document);
